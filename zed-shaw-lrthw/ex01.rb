c25acb907098bb056ea08d39a4639622301137f4
Amy McKnight <simplyamym@gmail.com>
2016-02-07 03:03:42 -0500

    renamed file to improve order

---
+++ zed-shaw-lrthw/ex01.rb
@@ -0,0 +1,19 @@
+#puts "Hello World!"
+puts "Hello Again"
+#puts "I like typing this."
+puts "This is fun."
+#puts "Yay! Printing."
+puts "I'd must rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "this is amazing!"
+
+# just retyping this to review it. 
+puts "_______________"
+puts "Hello World"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun"
+puts "Yay! Printing."
+puts "I'd much rather 'not'."
+puts 'I "said" do not touch this'
+
